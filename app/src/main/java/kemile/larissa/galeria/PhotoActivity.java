1db44868ff58e50af51c3af64c59349660cbfb2d
20201tiimi0217 <larissakemile24@gmail.com>
2022-09-16 09:58:19 -0300

    lala

--- app/src/main/java/kemile/larissa/galeria/PhotoActivity.java
+++ app/src/main/java/kemile/larissa/galeria/PhotoActivity.java
@@ -1,11 +1,13 @@
 package kemile.larissa.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -26,6 +28,16 @@ public class PhotoActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_tb, menu);
         return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
